f52a3eb067013784068e5d54a872b5673482869a
kndone <justinlim4916@gmail.com>
2024-05-23 22:10:46 +0800

    feat: show password button

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 
+const showPassword = ref(false)
+
 const username = ref('')
 const password = ref('')
 
@@ -14,9 +16,25 @@ function login() {
         <h2>Login</h2>
         <form class="login-form">
             <input type="text" v-model="username" name="username" placeholder="Username" />
-            <input type="password" v-model="password" name="password" placeholder="Password" />
+            <input
+                :type="showPassword ? 'text' : 'password'"
+                v-model="password"
+                name="password"
+                placeholder="Password"
+            />
+            <div class="login-show-password">
+                <label for="login-show-password">
+                    <input
+                        type="checkbox"
+                        v-model="showPassword"
+                        name="showPassword"
+                        id="login-show-password"
+                    />Show Password
+                </label>
+            </div>
             <button type="submit" @click.prevent="login">Login</button>
-            <small><a href="/register">Create Account</a></small>
+
+            <a href="/register">Create Account</a>
         </form>
     </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 
+const showPassword = ref(false)
+
 const email = ref('')
 const username = ref('')
 const password = ref('')
@@ -16,7 +18,22 @@ function register() {
         <form class="login-form">
             <input type="email" v-model="email" name="email" placeholder="DLSU Email" />
             <input type="text" v-model="username" name="username" placeholder="Username" />
-            <input type="password" v-model="password" name="password" placeholder="Password" />
+            <input
+                :type="showPassword ? 'text' : 'password'"
+                v-model="password"
+                name="password"
+                placeholder="Password"
+            />
+            <div class="login-show-password">
+                <label for="login-show-password">
+                    <input
+                        type="checkbox"
+                        v-model="showPassword"
+                        name="showPassword"
+                        id="login-show-password"
+                    />Show Password
+                </label>
+            </div>
             <button type="submit" @click.prevent="register">Register</button>
             <small><a href="/login">Login</a></small>
         </form>
